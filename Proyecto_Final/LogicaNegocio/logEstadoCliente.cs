b6fea1779ed0840b4b296844692e84a15af17767
Josejl <indestructible.123456@gmail.com>
2022-10-26 18:06:34 -0500

    .

--- Proyecto_Final/LogicaNegocio/logEstadoCliente.cs
+++ Proyecto_Final/LogicaNegocio/logEstadoCliente.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace entCiudad
+namespace LogicaNegocio
 {
-    class Class1
+    public class logEstadoCliente
     {
     }
 }

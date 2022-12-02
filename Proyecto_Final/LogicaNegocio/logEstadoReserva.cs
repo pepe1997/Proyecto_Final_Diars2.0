3c2e95a93947731fbb6b97d0e236c32145c82f74
Josejl <indestructible.123456@gmail.com>
2022-12-02 05:04:41 -0500

    .

---
+++ Proyecto_Final/LogicaNegocio/logEstadoReserva.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using entEstadoReserva;
+using AccesoDatos.DatReserva;
+
+namespace LogicaNegocio
+{
+    public class logEstadoReserva
+    {
+        #region singleton
+        private static readonly logEstadoReserva UnicaInstancia = new logEstadoReserva();
+        public static logEstadoReserva Instancia
+        {
+            get
+            {
+                return logEstadoReserva.UnicaInstancia;
+            }
+        }
+
+        #endregion singleton
+        #region metodos
+        public List<EstadoReserva> ListarEstReserva()
+        {
+            try
+            {
+                List<EstadoReserva> lista = datEstadoReserva.Instancia.ListarEstadoReserva();
+                return lista;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        public Boolean InsertarEstReserva(EstadoReserva Ser)
+        {
+            try
+            {
+                return datEstadoReserva.Instancia.InsertarEstadoReserva(Ser);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Boolean EditaEstadoReserva(EstadoReserva Ser)
+        {
+            try
+            {
+                return datEstadoReserva.Instancia.EditarEstadoReserva(Ser);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public EstadoReserva BuscarEstadoReserva(int idEstReserva)
+        {
+            try
+            {
+                return datEstadoReserva.Instancia.BuscarEstadoReserva(idEstReserva);
+            }
+            catch (Exception e)
+            { throw e; }
+
+        }
+        #endregion metodos
+    }
+}
---
+++ Proyecto_Final/LogicaNegocio/logReserva.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using entReserva;
+using AccesoDatos.DatReserva;
+
+namespace LogicaNegocio
+{
+    public class logReserva
+    {
+        #region singleton
+        private static readonly logReserva UnicaInstancia = new logReserva();
+        public static logReserva Instancia
+        {
+            get
+            {
+                return logReserva.UnicaInstancia;
+            }
+        }
+
+        #endregion singleton
+
+        #region metodos
+        public List<Reserva> ListarReserva()
+        {
+            try
+            {
+                List<Reserva> lista = datReserva.Instancia.ListarReserva();
+                return lista;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        public Boolean InsertarReserva(Reserva re)
+        {
+            try
+            {
+                return datReserva.Instancia.InsertarReserva(re);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Boolean EditaReserva(Reserva re)
+        {
+            try
+            {
+                return datReserva.Instancia.EditarReserva(re);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Reserva BuscarReserva(int idReserva)
+        {
+            try
+            {
+                return datReserva.Instancia.BuscarReserva(idReserva);
+            }
+            catch (Exception e)
+            { throw e; }
+
+        }
+        public Boolean EliminarReserva(Reserva c)
+        {
+            try
+            {
+                return datReserva.Instancia.EliminarReserva(c);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        #endregion metodos
+    }
+}
